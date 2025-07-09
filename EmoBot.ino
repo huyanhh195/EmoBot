dbb41160e2e08a24902c70192e56b53cf58dd212
huyanhh195 <huyanhh195@gmail.com>
2025-07-09 21:18:28 +0700

    module spi

--- EmoBot.ino
+++ EmoBot.ino
@@ -17,6 +17,110 @@ void blink_led(int time){
   digitalWrite(LED_PIN, LOW);
   delay(time);
 }
+
+void heart() {
+  display.clearDisplay();
+
+  int x1 = 44;
+  int y1 = 27;
+  int radius1 = 10;
+
+  // Draw the left heart
+  display.fillTriangle(x1 - radius1, y1, x1, y1 + radius1, x1 + radius1, y1, WHITE);
+  display.fillCircle(x1 - 5, y1, 5.5, WHITE);
+  display.fillCircle(x1 + 5, y1, 5.5, WHITE);
+
+  int x2 = 84;
+  int y2 = 27;
+  int radius2 = 10;
+
+  // Draw the right heart
+  display.fillTriangle(x2 - radius2, y2, x2, y2 + radius2, x2 + radius2, y2, WHITE);
+  display.fillCircle(x2 - 5, y2, 5.5, WHITE);
+  display.fillCircle(x2 + 5, y2, 5.5, WHITE);
+
+  //smile
+  display.fillCircle(64, 50, 12, WHITE);    // full smile
+  display.fillRect(0, 38, 128, 12, BLACK);  // hide top half of smile
+
+  display.display();
+  delay(4000);
+
+  //blink
+  display.clearDisplay();
+
+  //smallest mouth
+  display.fillRect(54, 54, 22, 4, WHITE);
+
+  display.fillRect(44, 28, 10, 4, WHITE);
+  display.fillRect(76, 28, 10, 4, WHITE);
+  display.display();
+  delay(100);
+  display.clearDisplay();
+
+  //smile
+  display.fillCircle(64, 50, 12, WHITE);    // full smile
+  display.fillRect(0, 38, 128, 12, BLACK);  // hide top half of smile
+
+
+  // Draw the left heart
+  display.fillTriangle(x1 - radius1, y1, x1, y1 + radius1, x1 + radius1, y1, WHITE);
+  display.fillCircle(x1 - 5, y1, 5.5, WHITE);
+  display.fillCircle(x1 + 5, y1, 5.5, WHITE);
+
+  // Draw the right heart
+  display.fillTriangle(x2 - radius2, y2, x2, y2 + radius2, x2 + radius2, y2, WHITE);
+  display.fillCircle(x2 - 5, y2, 5.5, WHITE);
+  display.fillCircle(x2 + 5, y2, 5.5, WHITE);
+
+  display.display();
+  delay(4000);
+
+  //blink
+  display.clearDisplay();
+  display.fillRect(44, 28, 10, 4, WHITE);
+  display.fillRect(76, 28, 10, 4, WHITE);
+
+  //smallest mouth
+  display.fillRect(54, 54, 22, 4, WHITE);
+
+  display.display();
+  delay(100);
+  display.clearDisplay();
+
+  // Draw the left heart
+  display.fillTriangle(x1 - radius1, y1, x1, y1 + radius1, x1 + radius1, y1, WHITE);
+  display.fillCircle(x1 - 5, y1, 5.5, WHITE);
+  display.fillCircle(x1 + 5, y1, 5.5, WHITE);
+
+  // Draw the right heart
+  display.fillTriangle(x2 - radius2, y2, x2, y2 + radius2, x2 + radius2, y2, WHITE);
+  display.fillCircle(x2 - 5, y2, 5.5, WHITE);
+  display.fillCircle(x2 + 5, y2, 5.5, WHITE);
+
+  //smile
+  display.fillCircle(64, 50, 12, WHITE);    // full smile
+  display.fillRect(0, 38, 128, 12, BLACK);  // hide top half of smile
+
+
+  display.display();
+  delay(4000);
+
+  //blink
+  display.clearDisplay();
+
+  display.clearDisplay();
+  display.fillRect(44, 28, 10, 4, WHITE);
+  display.fillRect(76, 28, 10, 4, WHITE);
+
+  //smallest mouth
+  display.fillRect(54, 54, 22, 4, WHITE);
+
+  display.display();
+  delay(100);
+  display.clearDisplay();
+}
+
 void blinkEye(){
   int leftX = 40;
   int rightX = 88;
@@ -64,6 +168,6 @@ void setup() {
 
 
 void loop() {
-  blinkEye();
+  heart();
 }
 
