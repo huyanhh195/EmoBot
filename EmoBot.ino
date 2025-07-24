97e6a191e22e0da39d1fae8b13e12ac2cf4ecb72
huyanhh195 <huyanhh195@gmail.com>
2025-07-24 16:22:04 +0700

    simple buzzer

--- EmoBot.ino
+++ EmoBot.ino
@@ -3,31 +3,70 @@
 void blink_led(int time);
 void playGIF(const AnimatedGIF* gif); 
 
+void setupI2S() {
+  i2s_config_t i2s_config = {
+    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
+    .sample_rate = SAMPLE_RATE,
+    .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
+    .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+    .communication_format = I2S_COMM_FORMAT_I2S_MSB,
+    .intr_alloc_flags = 0,
+    .dma_buf_count = 8,
+    .dma_buf_len = 64,
+    .use_apll = false,
+    .tx_desc_auto_clear = true,
+    .fixed_mclk = 0
+  };
+
+  i2s_pin_config_t pin_config = {
+    .bck_io_num = MAX98375A_BCLK,
+    .ws_io_num = MAX98375A_LRCLK,
+    .data_out_num = MAX98375A_DOUT,
+    .data_in_num = I2S_PIN_NO_CHANGE
+  };
+
+  i2s_driver_install(I2S_NUM, &i2s_config, 0, NULL);
+  i2s_set_pin(I2S_NUM, &pin_config);
+  i2s_zero_dma_buffer(I2S_NUM);
+}
+
+void playTone(float frequency, int duration_ms) {
+  if (frequency <= 0) {
+    delay(duration_ms);
+    return;
+  }
+
+  int samples_per_cycle = SAMPLE_RATE / frequency;
+  int cycles = (SAMPLE_RATE * duration_ms) / 1000 / samples_per_cycle;
+
+  int16_t sample_high = 16000;
+  int16_t sample_low = -16000;
+
+  size_t bytes_written;
+
+  for (int i = 0; i < cycles; ++i) {
+    for (int j = 0; j < samples_per_cycle; ++j) {
+      int16_t sample = (j < samples_per_cycle / 2) ? sample_high : sample_low;
+      i2s_write(I2S_NUM, (const char*)&sample, sizeof(sample), &bytes_written, portMAX_DELAY);
+    }
+  }
+}
+
 
 void setup() {
   Serial.begin(115200);
-  
-  pinMode(LED_PIN, OUTPUT);
-  
-  Wire.begin(SSD1306_SDA, SSD1306_SCL);
-  while(!display.begin(SSD1306_SWITCHCAPVCC, SSD1306_ADDR)){
-    Serial.println("Waiting Monitor");
-    blink_led(100);
-  }
+  delay(1000);
 
-  display.clearDisplay();
-  display.display();
-  playGIF(&EmoBot_Intro_gif, 50);
-  delay(500);
+  setupI2S();
+
 }
 
 void loop() {
-  playGIF(&Emobot_Default_gif, DELAY_FRAME);
-  delay(500);
-  playGIF(&EmoBot_Frustrated_gif, DELAY_FRAME);
-  delay(500);
-  playGIF(&EmoBot_Sleepy_gif, DELAY_FRAME);
-  delay(500);
+  for (int i = 0; melody[i].freq >= 0; i++) {
+    playTone(melody[i].freq, NOTE_DURATION_MS);
+    delay(50);  
+  }
+  delay(1000);
 }
 
 void blink_led(int time){
--- utils.h
+++ utils.h
@@ -1,6 +1,7 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <driver/i2s.h>
 
 #include "frames/EmoBot_Intro.h"
 #include "frames/Emobot_Default.h"
@@ -15,7 +16,28 @@
 #define SSD1306_WIDTH 128
 #define SSD1306_HEIGHT 64
 
+#define I2S_NUM         I2S_NUM_0  
+#define MAX98375A_BCLK        1         
+#define MAX98375A_LRCLK       0         
+#define MAX98375A_DOUT        2         
+
 #define DELAY_FRAME 100
 
 Adafruit_SSD1306 display(SSD1306_WIDTH, SSD1306_HEIGHT, &Wire);
 
+#define SAMPLE_RATE     44100
+#define NOTE_DURATION_MS 300
+
+struct Note {
+  const char* name;
+  float freq;
+};
+Note melody[] = {
+  {"C4",  261.63},
+  {"E4",  329.63},
+  {"G4",  392.00},
+  {"C5",  523.25},
+  {"E5",  659.25},
+  {"G5",  783.99},
+  {"REST", 0}
+};
