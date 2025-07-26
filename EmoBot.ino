70b2758d8a6bbae159c95752c00934165341e0f1
huyanhh195 <huyanhh195@gmail.com>
2025-07-26 12:04:25 +0700

    use open weather to read temp

--- EmoBot.ino
+++ EmoBot.ino
@@ -1,53 +1,48 @@
-#include "utils.h"
+#include <WiFi.h>
+#include <HTTPClient.h>
 
-void blink_led(int time);
-void playGIF(const AnimatedGIF* gif); 
+const char *ssid = "PQN";
+const char *password = "MKwifilagi";
+
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  
+  Serial.print("Connecting to WiFi ...\n");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+
+  Serial.print("\n");
+  Serial.println(WiFi.localIP());
+}
+
+void getTemp(){
+  HTTPClient http;
+  String server = "http://api.openweathermap.org/data/2.5/weather?q=Ho%20Chi%20Minh&appid=YOUR_API_KEY&units=metric";
+
+  http.begin(server);
+  int httpCode = http.GET();
+  if (httpCode > 0){
+    String payload = http.getString();
+    Serial.println(payload);
+  }
+  else{
+    Serial.println("Error on HTTP request");
+  }
+}
 
 void setup() {
   Serial.begin(115200);
-  
-  pinMode(LED_PIN, OUTPUT);
-  
-  Wire.begin(SSD1306_SDA, SSD1306_SCL);
-  while(!display.begin(SSD1306_SWITCHCAPVCC, SSD1306_ADDR)){
-    Serial.println("Waiting Monitor");
-    blink_led(100);
-  }
+  delay(1000);
 
-  display.clearDisplay();
-  display.display();
-  playGIF(&EmoBot_Intro_gif, 50);
-  delay(500);
+  initWiFi();
+  Serial.print("RRSI: ");
+  Serial.println(WiFi.RSSI());
 }
 
 void loop() {
-  for (int i = 0; i < 3; i ++){
-    playGIF(&gif[i], DELAY_FRAME);
-    delay(500);
-  }
-}
-void blink_led(int time){
-  digitalWrite(LED_PIN, HIGH);
-  delay(time);
-  digitalWrite(LED_PIN, LOW);
-  delay(time);
-}
-
-void playGIF(const AnimatedGIF* gif, uint16_t delays) {
-  for (uint8_t frame = 0; frame < gif->frame_count; frame++) {
-    display.clearDisplay();
-
-    for (uint16_t y = 0; y < gif->height; y++) {
-      for (uint16_t x = 0; x < gif->width; x++) {
-        uint16_t byteIndex = y * ((gif->width + 7) / 8) + (x / 8);
-        uint8_t bitIndex = 7 - (x % 8);
-        if (pgm_read_byte(&gif->frames[frame][byteIndex]) & (1 << bitIndex)) {
-          display.drawPixel(x, y, SSD1306_WHITE);
-        }
-      }
-    }
-
-    display.display();
-    delay(delays);
-  }
+  getTemp();
+  delay(1000);
 }
