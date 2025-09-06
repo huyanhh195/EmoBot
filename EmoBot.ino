6566c989bb9be48382ea85780779c0f50b6221ae
huyanhh195 <huyanhh195@gmail.com>
2025-09-06 08:21:06 +0700

    play wav

--- EmoBot.ino
+++ EmoBot.ino
@@ -1,7 +1,47 @@
 #include "utils.h"
 
 void blink_led(int time);
-void playGIF(const AnimatedGIF* gif); 
+void setupI2S();
+
+const int wav_data_start = 44;
+const int wav_data_len = sizeof(Emo_default_wav) - wav_data_start;
+
+
+void playWav(const unsigned char* wav, unsigned int len,  float volume_factor = 5.0) {
+  size_t bytes_written = 0;
+  unsigned int pos = 44;
+  const int chunk = DMA_BUF_LEN;
+
+  while (pos < len) {
+    int to_write = (len - pos) > chunk ? chunk : (len - pos);
+    int16_t temp_buf[DMA_BUF_LEN / 2]; 
+
+    for (int i = 0; i < to_write / 2; i++) {
+      int16_t sample = ((int16_t*)wav)[(pos / 2) + i]
+      int32_t amplified = (int32_t)(sample * volume_factor);
+      if (amplified > 32767) amplified = 32767;
+      if (amplified < -32768) amplified = -32768;
+      temp_buf[i] = (int16_t)amplified;
+    }
+
+    i2s_write(I2S_NUM_0, temp_buf, to_write, &bytes_written, portMAX_DELAY);
+
+    pos += bytes_written;
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  setupI2S();
+
+  playWav(Emo_default_wav, Emo_default_wav_len, 10);
+}
+
+void loop() {
+
+}
 
 void setupI2S() {
   i2s_config_t i2s_config = {
@@ -12,7 +52,7 @@ void setupI2S() {
     .communication_format = I2S_COMM_FORMAT_I2S_MSB,
     .intr_alloc_flags = 0,
     .dma_buf_count = 8,
-    .dma_buf_len = 64,
+    .dma_buf_len = DMA_BUF_LEN,
     .use_apll = false,
     .tx_desc_auto_clear = true,
     .fixed_mclk = 0
@@ -25,72 +65,7 @@ void setupI2S() {
     .data_in_num = I2S_PIN_NO_CHANGE
   };
 
-  i2s_driver_install(I2S_NUM, &i2s_config, 0, NULL);
-  i2s_set_pin(I2S_NUM, &pin_config);
-  i2s_zero_dma_buffer(I2S_NUM);
-}
-
-void playTone(float frequency, int duration_ms) {
-  if (frequency <= 0) {
-    delay(duration_ms);
-    return;
-  }
-
-  int samples_per_cycle = SAMPLE_RATE / frequency;
-  int cycles = (SAMPLE_RATE * duration_ms) / 1000 / samples_per_cycle;
-
-  int16_t sample_high = 16000;
-  int16_t sample_low = -16000;
-
-  size_t bytes_written;
-
-  for (int i = 0; i < cycles; ++i) {
-    for (int j = 0; j < samples_per_cycle; ++j) {
-      int16_t sample = (j < samples_per_cycle / 2) ? sample_high : sample_low;
-      i2s_write(I2S_NUM, (const char*)&sample, sizeof(sample), &bytes_written, portMAX_DELAY);
-    }
-  }
-}
-
-
-void setup() {
-  Serial.begin(115200);
-  delay(1000);
-
-  setupI2S();
-
-}
-
-void loop() {
-  for (int i = 0; melody[i].freq >= 0; i++) {
-    playTone(melody[i].freq, NOTE_DURATION_MS);
-    delay(50);  
-  }
-  delay(1000);
-}
-
-void blink_led(int time){
-  digitalWrite(LED_PIN, HIGH);
-  delay(time);
-  digitalWrite(LED_PIN, LOW);
-  delay(time);
-}
-
-void playGIF(const AnimatedGIF* gif, uint16_t delays) {
-  for (uint8_t frame = 0; frame < gif->frame_count; frame++) {
-    display.clearDisplay();
-
-    for (uint16_t y = 0; y < gif->height; y++) {
-      for (uint16_t x = 0; x < gif->width; x++) {
-        uint16_t byteIndex = y * ((gif->width + 7) / 8) + (x / 8);
-        uint8_t bitIndex = 7 - (x % 8);
-        if (pgm_read_byte(&gif->frames[frame][byteIndex]) & (1 << bitIndex)) {
-          display.drawPixel(x, y, SSD1306_WHITE);
-        }
-      }
-    }
-
-    display.display();
-    delay(delays);
-  }
+  i2s_driver_install(I2S_NUM_0, &i2s_config, 0, NULL);
+  i2s_set_pin(I2S_NUM_0, &pin_config);
+  i2s_zero_dma_buffer(I2S_NUM_0);
 }
--- utils.h
+++ utils.h
@@ -3,10 +3,7 @@
 #include <Adafruit_SSD1306.h>
 #include <driver/i2s.h>
 
-#include "frames/EmoBot_Intro.h"
-#include "frames/Emobot_Default.h"
-#include "frames/EmoBot_Sleepy.h"
-#include "frames/EmoBot_Frustrated.h"
+#include "sounds/Emo_default_wav.h"
 
 #define LED_PIN 8
 
@@ -16,28 +13,14 @@
 #define SSD1306_WIDTH 128
 #define SSD1306_HEIGHT 64
 
-#define I2S_NUM         I2S_NUM_0  
 #define MAX98375A_BCLK        1         
 #define MAX98375A_LRCLK       0         
 #define MAX98375A_DOUT        2         
+#define DMA_BUF_LEN 64
 
 #define DELAY_FRAME 100
 
 Adafruit_SSD1306 display(SSD1306_WIDTH, SSD1306_HEIGHT, &Wire);
 
-#define SAMPLE_RATE     44100
+#define SAMPLE_RATE     8000
 #define NOTE_DURATION_MS 300
-
-struct Note {
-  const char* name;
-  float freq;
-};
-Note melody[] = {
-  {"C4",  261.63},
-  {"E4",  329.63},
-  {"G4",  392.00},
-  {"C5",  523.25},
-  {"E5",  659.25},
-  {"G5",  783.99},
-  {"REST", 0}
-};
